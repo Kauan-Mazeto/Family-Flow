752a8a550fc3f0bddc18ba01b10999291adbb1b1
FireWater11 <luidi.augusto1234@gmail.com>
2025-10-28 19:45:51 -0300

    feat: back (route users)

---
+++ Back-End/src/middlewares/authToken.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET_KEY = process.env.PASS_HASH;
+
+export function authToken(req, res, next) {
+    const token = req.cookies.tokenAuth;
+
+    if (!token) {
+        return res.status(401).json({ mensagem: "Token inválido ou inexistente" });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
+        req.usuario = decodedToken; // Anexando dados do usuário à requisição
+        
+        next();
+
+    } catch (err) {
+        return res.status(401).json({ mensagem: "Token inválido ou expirado" });
+    }
+}
---
+++ Back-End/src/tst.js
@@ -0,0 +1,11 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// // await prisma.$executeRawUnsafe(`DELETE FROM User`);
+
+// // await prisma.$executeRawUnsafe(`DELETE FROM sqlite_sequence WHERE name='User'`);
+
+// // Apague todos os usuários e recadastre
+// await prisma.user.deleteMany({});
+// await prisma.$executeRawUnsafe(`DELETE FROM sqlite_sequence WHERE name='User'`);
