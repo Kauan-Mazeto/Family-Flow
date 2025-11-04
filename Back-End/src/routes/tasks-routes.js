15e181ae55d064979516d30906d72ee2c21f344c
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-03 23:34:32 -0300

    create route /tasks/daily

--- Back-End/src/routes/tasks-routes.js
+++ Back-End/src/routes/tasks-routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authRole } from '../middlewares/authRole.js';
+import { authToken } from '../middlewares/authToken.js';
 // import { authToken } from '../middlewares/authToken.js';
 
-rotas_tasks.post('/tasks/daily', authRole, (req, res) => {
-
-});
-
 const rotas_tasks = express();
+
+rotas_tasks.post('/tasks/daily', authToken, authRole, (req, res) => {
+    
+});
 
 export default rotas_tasks;
