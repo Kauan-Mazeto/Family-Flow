9fcc338ec01de0fa72b5154bc5c09a2e3663cc08
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-04 01:35:58 -0300

    feat: function family_id_task + usuario_atual_id

--- Back-End/src/controller/functions-controller.js
+++ Back-End/src/controller/functions-controller.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import member_task from "./tasks-controller.js";
 
 const prisma = new PrismaClient();
 
@@ -27,5 +28,39 @@ export async function usuario_atual(idUsuario) {
 
     } catch (err) {
         console.error(err); 
+    };
+};
+
+export async function usuario_atual_id(idUsuario) {
+
+    try {
+        const info_member = await prisma.user.findFirst({
+            where: {
+                name: member_task
+            },
+
+            select: {
+                id: true
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    };
+};
+
+export async function family_id_task(idUsuario) {
+    
+    try {
+        const info_family = await prisma.FamilyMember.findFirst({
+            where: {
+                user_id: idUsuario
+            },
+
+            select: {
+                family_id: true
+            }
+        });
+    } catch (err) {
+        console.error(err)
     };
 };
