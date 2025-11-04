3020897aa38dbc43bda218f0e91b75d7b96666a4
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-04 17:00:38 -0300

    execution tst.js => delete db

--- Back-End/src/tst.js
+++ Back-End/src/tst.js
@@ -2,9 +2,9 @@
 
 // const prisma = new PrismaClient();
 
-// // await prisma.$executeRawUnsafe(`DELETE FROM User`);
+// await prisma.$executeRawUnsafe(`DELETE FROM User`);
 
-// // await prisma.$executeRawUnsafe(`DELETE FROM sqlite_sequence WHERE name='User'`);
+// await prisma.$executeRawUnsafe(`DELETE FROM sqlite_sequence WHERE name='User'`);
 
 // // Apague todos os usuários e recadastre
 // await prisma.user.deleteMany({});
