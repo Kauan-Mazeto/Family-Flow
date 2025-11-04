1a51c121c1fde14fe048772c40d76fa1054e4a3e
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-04 01:37:19 -0300

    fix: create tasks in model Task

--- Back-End/src/controller/tasks-controller.js
+++ Back-End/src/controller/tasks-controller.js
@@ -1,6 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import { family_id_task, usuario_atual_id } from './functions-controller';
 
 const prisma = new PrismaClient();
+const id_member = usuario_atual_id(req.usuario.id);
+const id_family = family_id_task(req.usuario.id);
 
 export async function task_adm(req, res) {
     const { desc_task, name_task, member_task, priority_task, status_task, type_task } = req.body;
@@ -13,8 +16,14 @@ export async function task_adm(req, res) {
 
         const task_info = await prisma.task.create({
             data: {
-                type_task: type_task,
-                
+                description: desc_task,
+                title: name_task,
+                family_id: id_family,
+                member_id: id_member,
+                member_name: member_task,
+                priority: priority_task,
+                status: status_task,
+                type_task: type_task
             }
         });
 
@@ -23,3 +32,5 @@ export async function task_adm(req, res) {
         console.error(err);
     };
 };
+
+export default member_task;
