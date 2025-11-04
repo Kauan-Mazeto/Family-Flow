a81ff44d7f04eaf85beebc3b3731ccd1541fa636
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-04 14:56:04 -0300

    complete function => task_adm + return logic correct

--- Back-End/src/controller/tasks-controller.js
+++ Back-End/src/controller/tasks-controller.js
@@ -1,16 +1,25 @@
 import { PrismaClient } from '@prisma/client';
-import { family_id_task, usuario_atual_id } from './functions-controller';
+import { family_id_task, usuario_atual_id } from './functions-controller.js';
+
 
 const prisma = new PrismaClient();
-const id_member = usuario_atual_id(req.usuario.id);
-const id_family = family_id_task(req.usuario.id);
 
 export async function task_adm(req, res) {
+
     const { desc_task, name_task, member_task, priority_task, status_task, type_task } = req.body;
 
     if (!desc_task || !name_task || !member_task || !priority_task || !status_task || !type_task) {
         return res.status(404).json({ mensagem: "Informações obrigatórias." });
     };
+
+    const id_member = await usuario_atual_id(member_task);
+
+    if (!id_member) {
+        return res.status(404).json({ mensagem: "Membro não encontrado" });
+    };
+
+    const id_family = await family_id_task(id_member);
+    
 
     try {
 
@@ -18,19 +27,28 @@ export async function task_adm(req, res) {
             data: {
                 description: desc_task,
                 title: name_task,
-                family_id: id_family,
-                member_id: id_member,
+                // family_id: Number(id_family),
+                member_id: Number(id_member),
                 member_name: member_task,
                 priority: priority_task,
                 status: status_task,
-                type_task: type_task
+                type_task: type_task,
+                family: {
+                    connect: { 
+                        id: Number(id_family) 
+                    }
+                }
             }
         });
+
+        return res.status(201).json({
+                mensagem: "Task criada.",  
+                task_info
+            }
+        );
 
     } catch (err) {
         res.status(500).json({ mensagem: "Erro interno no servidor." });
         console.error(err);
     };
 };
-
-export default member_task;
