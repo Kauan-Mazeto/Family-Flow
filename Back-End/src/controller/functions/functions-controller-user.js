0ab13333b0cd2e1e9e654ee642af2656761009a5
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-05 15:42:35 -0300

    create paste for functions-user

---
+++ Back-End/src/controller/functions/functions-controller-user.js
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function usuario_atual(idUsuario) {
+
+    try {
+
+        const user_active = await prisma.FamilyMember.findFirst({
+            where: {
+                user_id: Number(idUsuario)
+            },
+
+            select: {
+                family_id: true,
+                id: true,
+                role: true,
+            }
+        });
+
+        if (!user_active) {
+            return { mensagem: "Este usuário não está em nenhuma família." };
+        };
+
+        return user_active;
+
+
+    } catch (err) {
+        console.error(err); 
+    };
+};
+
+export async function usuario_atual_id(nomeMembro) {
+
+    try {
+        const info_member = await prisma.user.findFirst({
+            where: {
+                name: nomeMembro
+            },
+
+            select: {
+                id: true
+            }
+        });
+
+        return info_member?.id || null;
+
+    } catch (err) {
+        console.error(err);
+    };
+};
+
+export async function usuario_atual_nome(idUsuario) {
+    const info_user_name = await prisma.user.findFirst({
+        where: {
+            id: idUsuario
+        },
+
+        select: {
+            name: true
+        }
+    });
+
+    return info_user_name;
+};
