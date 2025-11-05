f49053aa69fdf582e982c9999dc3d585ae7128e3
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-05 14:54:54 -0300

    cleanCode changes in view code

--- Back-End/src/controller/tasks-controller.js
+++ Back-End/src/controller/tasks-controller.js
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { family_id_task, usuario_atual_id } from './functions/functions-controller.js';
 
-
 const prisma = new PrismaClient();
 
 export async function task_adm(req, res) {
@@ -57,50 +56,6 @@ export async function task_adm(req, res) {
     };
 };
 
-export async function task_users_create(req, res) {
-    const { desc_task, name_task, priority_task, status_task, type_task } = req.body;
-    // desc_task: descricao da tarefa
-    // name_task: nome da tarefa
-    // member_task: sempre vai ser o usuario que esta logado
-    // priority_task: prioridade da tarefa
-    // status_task: status da tarefa
-    // type_task: tipo da tarefa(diaria/pontual)
-
-    if (!desc_task || !name_task || !priority_task || !status_task || !type_task) {
-        return res.status(404).json({ mensagem: "Informações obrigatórias." });
-    };
-
-    const id_family = await family_id_task(req.usuario.id);
-    const priority_upperCase = priority_task.toUpperCase();
-    const status_upperCase = status_task.toUpperCase();
-
-    try {
-        const task_info = await prisma.task.create({
-            data: {
-                description: desc_task,
-                title: name_task,
-                member_id: Number(req.usuario.id),
-                priority: priority_upperCase,
-                status: status_upperCase,
-                type_task: type_task,
-                family: {
-                    connect: { 
-                        id: Number(id_family) 
-                    }
-                }
-            }
-        });
-
-        return res.status(200).json({
-            message: "Task exclusiva criada.",
-            task_info
-        });
-
-    } catch (err) {
-        res.status(500).json({ mensagem: "Erro interno no servidor." });
-        console.error(err);
-    };
-};
 
 export async function remove_task_adm(req, res) {
     const { task_remove } = req.body;
@@ -161,3 +116,52 @@ export async function remove_task_adm(req, res) {
 //         console.error(err);
 //     };
 // };
+
+export async function task_user_create(req, res) {
+    const { desc_task, name_task, priority_task, status_task, type_task } = req.body;
+    // desc_task: descricao da tarefa
+    // name_task: nome da tarefa
+    // member_task: sempre vai ser o usuario que esta logado
+    // priority_task: prioridade da tarefa
+    // status_task: status da tarefa
+    // type_task: tipo da tarefa(diaria/pontual)
+
+    if (!desc_task || !name_task || !priority_task || !status_task || !type_task) {
+        return res.status(404).json({ mensagem: "Informações obrigatórias." });
+    };
+
+    const id_family = await family_id_task(req.usuario.id);
+    const priority_upperCase = priority_task.toUpperCase();
+    const status_upperCase = status_task.toUpperCase();
+
+    try {
+        const task_info = await prisma.task.create({
+            data: {
+                description: desc_task,
+                title: name_task,
+                member_id: Number(req.usuario.id),
+                priority: priority_upperCase,
+                status: status_upperCase,
+                type_task: type_task,
+                family: {
+                    connect: { 
+                        id: Number(id_family) 
+                    }
+                }
+            }
+        });
+
+        return res.status(200).json({
+            message: "Task exclusiva criada.",
+            task_info
+        });
+
+    } catch (err) {
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
+        console.error(err);
+    };
+};
+
+export async function get_task_user(params) {
+    
+};
