8fb486359087063778084bf05c9a48fd69ddacab
FireWater11 <luidi.augusto1234@gmail.com>
2025-11-05 20:17:32 -0300

    feat: task /tasks/update/:id => method PATCH

--- Back-End/src/routes/tasks-routes.js
+++ Back-End/src/routes/tasks-routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authRole } from '../middlewares/authRole.js';
 import { authToken } from '../middlewares/authToken.js';
-import { task_adm, remove_task_adm, create_task_user, get_task_user } from '../controller/tasks-controller.js';
+import { task_adm, remove_task_adm, create_task_user, get_task_user, patch_task_adm } from '../controller/tasks-controller.js';
 
 const rotas_tasks = express();
 
@@ -11,6 +11,10 @@ rotas_tasks.post('/tasks/create/daily', authToken, authRole, (req, res) => {
 
 rotas_tasks.delete('/tasks/delete', authToken, authRole, (req, res) => {
     remove_task_adm(req, res);
+});
+    
+rotas_tasks.patch('/tasks/update/:id', authToken, (req, res) => {
+    patch_task_adm(req, res);
 });
 
 rotas_tasks.post('/tasks/create/ponctual', authToken, (req, res) => {
