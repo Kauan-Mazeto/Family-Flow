c7300abc5bc3759abed77c289d0d30092138b35a
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-11-06 16:35:45 -0300

    create route /tasks/diaries/delete/:id

--- Back-End/src/routes/tasks-routes.js
+++ Back-End/src/routes/tasks-routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { authRole } from '../middlewares/authRole.js';
 import { authToken } from '../middlewares/authToken.js';
 import { task_adm, remove_task_adm, patch_task_adm } from '../controller/tasks/tasks-controller-admin.js';
-import { create_task_user, get_task_user } from '../controller/tasks/tasks-controller-anyone.js';
+import { create_task_user, get_task_user, remove_task_user } from '../controller/tasks/tasks-controller-anyone.js';
+import { authTaskMember } from '../middlewares/authTaskMember.js';
 
 const rotas_tasks = express();
 
@@ -10,8 +11,12 @@ rotas_tasks.post('/tasks/create/daily', authToken, authRole, (req, res) => {
     task_adm(req, res);
 });
 
-rotas_tasks.delete('/tasks/delete', authToken, authRole, (req, res) => {
+rotas_tasks.delete('/tasks/ponctual/delete', authToken, authRole, (req, res) => {
     remove_task_adm(req, res);
+});
+
+rotas_tasks.delete('/tasks/diaries/delete/:id', authToken, authTaskMember, (req, res) => {
+    remove_task_user(req, res);
 });
     
 rotas_tasks.patch('/tasks/update/:id', authToken, (req, res) => {
