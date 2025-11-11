cfa92c94763cef6353cdb33c085f4cf272f2d8fb
FireWater11 <luidi.augusto1234@gmail.com>
2025-11-10 22:27:44 -0300

    refactor message errors

--- Back-End/src/routes/login-routes.js
+++ Back-End/src/routes/login-routes.js
@@ -26,7 +26,7 @@ rotas_usuario.post('/users/logout', authToken, (req, res) => {
 
 rotas_usuario.post('/users/verify', authToken, (req, res) => {
     res.json({
-        mensagem: "Você esta autenticado.",
+        mensagem: "Você está autenticado!",
         usuario: req.usuario
     });
 });
